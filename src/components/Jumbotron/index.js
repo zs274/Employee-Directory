1778a9ee8c78a56cec996f70401ca7fa659306d2
Zarin Salim <zarin_96@hotmail.co.uk>
2021-05-04 16:42:58 +0100

    created jumbotron

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+    return (
+     <div className="jumbotron fluid">
+         <div className="container">
+             <h1>Employee Directory</h1>
+         </div>
+     </div>
+    )}
+
+export default Jumbotron;
