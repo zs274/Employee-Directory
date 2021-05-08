3a555c53f567edd67c68a4788a4a19ab205521e0
Zarin Salim <zarin_96@hotmail.co.uk>
2021-05-08 02:22:09 +0100

    created table, but not displaying on page, need to fix app js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Jumbotron from './components/Jumbotron/index.js';
+// import Container from './components/Container/index.js';
+import Table from './components/Table/index.js';
 
 function App() {
   return (
-   <Jumbotron />
+    <Router>
+      <div>
+        {/* <Container> */}
+          <Jumbotron />
+          <Route exact path ="/" component={Table} />
+        {/* </Container> */}
+      </div>
+    </Router>
+
+
   );
 }
 
