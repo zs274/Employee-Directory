c38374815fcc8e9330af668a8a0ff98e5195b3cb
Zarin Salim <zarin_96@hotmail.co.uk>
2021-05-09 15:09:55 +0100

    updated link to API

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,8 +1,9 @@
 import axios from "axios";
-const URL = "https://randomuser.me/api/?results=100&nat=gb";
 
-export default {
+const API = {
     getUsers: function() {
-        return axios.get(URL);
-    }
+        return axios.get("https://randomuser.me/api/?results=100&nat=gb");
+    },
 };
+
+export default API;
