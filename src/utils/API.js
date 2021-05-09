91d00d2d521d3366375e1e2d13d6759495096b09
Zarin Salim <zarin_96@hotmail.co.uk>
2021-05-09 01:28:05 +0100

    updated api

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-const URL = "https://randomuser.me/api/?inc=phone,name,email,picture";
+const URL = "https://randomuser.me/api/?results=100&nat=gb";
 
 export default {
-    search: function() {
+    getUsers: function() {
         return axios.get(URL);
     }
 };
