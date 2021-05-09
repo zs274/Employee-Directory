fd9a65f23b523a621bb69c4c8e1ac2e0810a5264
Zarin Salim <zarin_96@hotmail.co.uk>
2021-05-09 15:11:57 +0100

    created navbar to hold search bar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import SearchName from "../SearchName";
+
+import "./style.css";
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+
+       <div className="search-area col-4">
+        <SearchName />
+    </div>
+</nav>
+    );
+}
+export default Navbar;
